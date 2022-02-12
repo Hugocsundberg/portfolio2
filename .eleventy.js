d87a286ed6c905844cdb33a765f24f3f251bef84
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2022-02-12 19:20:14 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function (eleventyConfig) {
+  // Return your Object options:
+  eleventyConfig.addFilter("dump", (obj) => {
+    return util.inspect(obj);
+  });
+
+  eleventyConfig.addPassthroughCopy("node_modules/animejs/lib/anime.es.js");
+};
---
+++ script.ts
@@ -0,0 +1,13 @@
+import anime from "./node_modules/animejs/lib/anime.es.js";
+
+anime({
+  targets: "div",
+  translateX: 250,
+  translateY: 250,
+  scale: 5,
+  rotate: "1turn",
+  duration: 800,
+});
+
+const string: string = "string";
+console.log(string);
