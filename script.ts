5bb6a78864644ad1515b90cdd980273b1ccddc6e
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2022-02-13 20:15:44 +0100

    Update landing / navigation

--- script.ts
+++ script.ts
@@ -1,13 +1,47 @@
+// @ts-ignore
 import anime from "./node_modules/animejs/lib/anime.es.js";
 
-anime({
-  targets: "div",
-  translateX: 250,
-  translateY: 250,
-  scale: 5,
-  rotate: "1turn",
-  duration: 800,
+// Split portfolio string into individual characters.
+const h1 = document.querySelector("h1");
+if (h1) {
+  const h1Array = h1.innerText.split("");
+  // Wrap each character in a span tag
+  const h1SpanArray = h1Array.map((item) => `<span>${item}</span>`);
+  const h1SpanString = h1SpanArray.join("");
+  // Replace h1 with span-wrapped content
+  h1.innerHTML = h1SpanString;
+}
+
+// Animation
+const tl = anime.timeline();
+
+tl.add({
+  targets: ".animate-in",
+  translateY: [-window.innerHeight, 0],
+  scale: [2, 1],
+  delay: anime.stagger(100),
+  easing: "spring(3, 100, 17, 1)",
 });
 
-const string: string = "string";
-console.log(string);
+tl.add(
+  {
+    targets: "h1 span",
+    color: ["rgb(0, 0, 0)", "rgb(255, 255, 255)", "rgb(0, 0, 0)"],
+    easing: "spring(3, 100, 17, 1)",
+    delay: anime.stagger(75),
+  },
+  "-=900"
+);
+
+// Scroll to top arrow
+const scrollToTopButton: HTMLButtonElement | null = document.querySelector(
+  "button.scroll-to-top"
+);
+
+if (scrollToTopButton) {
+  window.addEventListener("touchstart", (e) => {
+    e.touches[0].clientX < window.innerWidth / 2
+      ? scrollToTopButton.classList.remove("move-right")
+      : scrollToTopButton.classList.add("move-right");
+  });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
   content: ["**/*.njk"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        gradient: "gradient 15s ease infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%": {
+            "background-position": "0% 50%",
+          },
+          "50%": {
+            "background-position": "100% 50%",
+          },
+          "100%": {
+            "background-position": "0% 50%",
+          },
+        },
+      },
+      backgroundSize: {
+        gradient: "400% 400%",
+      },
+      backgroundImage: {
+        gradient:
+          "linear-gradient(119deg, rgba(255,255,255,1) 17%, rgba(206,247,255,1) 57%, rgba(251,255,162,1) 100%);",
+      },
+    },
   },
   plugins: [],
 };
