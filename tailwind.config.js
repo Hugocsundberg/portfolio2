2f771f9dcf3c798af092d71dea6d8dc82da1bb27
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2022-02-14 21:59:31 +0100

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,17 @@ module.exports = {
         gradient:
           "linear-gradient(119deg, rgba(255,255,255,1) 17%, rgba(206,247,255,1) 57%, rgba(251,255,162,1) 100%);",
       },
+      borderRadius: {
+        "card-medium": "0 20px 20px 0",
+        "card-small": "0 10px 10px 0",
+        "icon-medium": "0 11px 11px 0",
+        "icon-small": "0 7px 7px 0",
+      },
+      spacing: {
+        small: "32px",
+        medium: "48px",
+        large: "80px",
+      },
     },
   },
   plugins: [],
