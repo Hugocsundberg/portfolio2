0ebd3092a323d70464450338aa29a0fbc19b986d
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2022-02-20 14:38:35 +0100

    fix: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,8 +26,10 @@ module.exports = {
           "linear-gradient(119deg, rgba(255,255,255,1) 17%, rgba(206,247,255,1) 57%, rgba(251,255,162,1) 100%);",
       },
       borderRadius: {
-        "card-medium": "0 20px 20px 0",
-        "card-small": "0 10px 10px 0",
+        "image-medium": "0 20px 20px 0",
+        "image-small": "0 10px 10px 0",
+        "card-medium": "20px",
+        "card-small": "10px",
         "icon-medium": "0 11px 11px 0",
         "icon-small": "0 7px 7px 0",
       },
@@ -35,7 +37,34 @@ module.exports = {
         small: "32px",
         medium: "48px",
         large: "80px",
+        "card-small": "16px",
+        "card-medium": "24px",
+        "card-large": "40px",
       },
+      boxShadow: {
+        card: "0px 4px 100px -20px rgba(0, 0, 0, 0.3)",
+        "card-small": "0px 4px 32px -19px rgba(0, 0, 0, 1)",
+        "card-smaller": "0px 4px 20px -10px rgba(0, 0, 0, 1);",
+      },
+    },
+    screens: {
+      xs: "340px",
+      // => @media (min-width: 340px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
